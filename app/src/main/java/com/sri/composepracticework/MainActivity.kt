fa79d604bd3f4aae9658ee8b5022b1abac9dc008
srikanth4mobileapps <srikanth4mobileapps@gmail.com>
2024-04-05 10:53:38 +0800

    Revert "Box layout with simple text"
    This reverts commit 5de5f6fbb7876c2a2ef5dd7fbc9aced2e86544b8.

--- app/src/main/java/com/sri/composepracticework/MainActivity.kt
+++ app/src/main/java/com/sri/composepracticework/MainActivity.kt
@@ -3,15 +3,12 @@ package com.sri.composepracticework
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.sri.composepracticework.ui.theme.ComposePracticeWorkTheme
 
@@ -25,97 +22,25 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Srikanth")
+                    Greeting("Android")
                 }
             }
         }
     }
 }
 
-
 @Composable
-fun Greeting(name: String) {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.CenterStart) {
-        Text(
-            text = " $name",
-            color = androidx.compose.ui.graphics.Color.Red,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(
-                Alignment.TopStart
-            )
-        )
-        Text(
-            text = "Greetings $name",
-            color = androidx.compose.ui.graphics.Color.Red,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(
-                Alignment.TopCenter
-            )
-        )
-        Text(
-            text = " $name",
-            color = androidx.compose.ui.graphics.Color.Red,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(
-                Alignment.TopEnd
-            )
-        )
-        Text(
-            text = " $name",
-            color = androidx.compose.ui.graphics.Color.Red,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(
-                Alignment.CenterStart
-            )
-        )
-        Text(
-            text = "Greetings $name",
-            color = androidx.compose.ui.graphics.Color.Red,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(
-                Alignment.Center
-            )
-        )
-        Text(
-            text = " $name",
-            color = androidx.compose.ui.graphics.Color.Red,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(
-                Alignment.CenterEnd
-            )
-        )
-        Text(
-            text = " $name",
-            color = androidx.compose.ui.graphics.Color.Red,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(
-                Alignment.BottomStart
-            )
-        )
-        Text(
-            text = "Greetings $name",
-            color = androidx.compose.ui.graphics.Color.Red,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(
-                Alignment.BottomCenter
-            )
-        )
-        Text(
-            text = " $name",
-            color = androidx.compose.ui.graphics.Color.Red,
-            fontWeight = FontWeight.Bold,
-            modifier = Modifier.align(
-                Alignment.BottomEnd
-            )
-        )
-    }
-
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ComposePracticeWorkTheme {
-        Greeting("Srikanth")
+        Greeting("Android")
     }
 }
