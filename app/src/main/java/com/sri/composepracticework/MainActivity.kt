685fe7e004ac73042b67ef2ebc91ada0ca481a56
srikanth4mobileapps <srikanth4mobileapps@gmail.com>
2024-04-05 11:09:52 +0800

    Row layout with simple text

--- app/src/main/java/com/sri/composepracticework/MainActivity.kt
+++ app/src/main/java/com/sri/composepracticework/MainActivity.kt
@@ -3,11 +3,14 @@ package com.sri.composepracticework
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.sri.composepracticework.ui.theme.ComposePracticeWorkTheme
@@ -31,10 +34,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Row(modifier = Modifier.fillMaxSize(), horizontalArrangement = Arrangement.SpaceEvenly, verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            text = "$name!",
+            modifier = modifier
+        )
+        Text(
+            text = "$name!",
+            modifier = modifier
+        )
+        Text(
+            text = "$name!",
+            modifier = modifier
+        )
+    }
+
 }
 
 @Preview(showBackground = true)
