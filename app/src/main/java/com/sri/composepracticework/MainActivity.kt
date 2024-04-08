4950dfd2f6623a314c2a3a7df987fff8b8fea2c8
srikanth4mobileapps <srikanth4mobileapps@gmail.com>
2024-04-08 09:27:59 +0800

    Textfield implemented using jetpack compose

--- app/src/main/java/com/sri/composepracticework/MainActivity.kt
+++ app/src/main/java/com/sri/composepracticework/MainActivity.kt
@@ -1,14 +1,41 @@
 package com.sri.composepracticework
 
 import android.os.Bundle
+import android.view.animation.Transformation
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import com.sri.composepracticework.ui.theme.ComposePracticeWorkTheme
 
@@ -17,12 +44,43 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeWorkTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                var text by remember {
+                    mutableStateOf("")
+                }
+                Column(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
                 ) {
-                    Greeting("Android")
+                    TextField(value = text,
+                        onValueChange = { text = it },
+                        leadingIcon = {
+                            Icon(imageVector = Icons.Default.Person, contentDescription = null)
+                        },
+                        trailingIcon = {
+                           // Icon(imageVector = Icons.Default.PlayArrow, contentDescription =null )
+                            TextButton(onClick = { /*TODO*/ }) {
+                                Text(text = "Show")
+                            }
+                        },
+                        colors = TextFieldDefaults.colors(unfocusedContainerColor = Color.Transparent, focusedContainerColor = Color.Transparent, unfocusedIndicatorColor = Color.Black, focusedIndicatorColor = Color.Black),
+                        singleLine = true,
+                       /* placeholder = {
+                            Text(text = "Enter you name")
+                        }*/
+                        label = {
+                            Text(text = "Enter you name")
+                        },
+                        visualTransformation = PasswordVisualTransformation(),
+                        keyboardOptions = KeyboardOptions(
+                            autoCorrect = false,
+                            keyboardType = KeyboardType.NumberPassword,
+                            imeAction = ImeAction.Go
+                        ),
+                       keyboardActions =  KeyboardActions(onGo = {
+                           Toast.makeText(this@MainActivity, "on GO Clicked", Toast.LENGTH_SHORT).show()
+                       })
+                    )
                 }
             }
         }
